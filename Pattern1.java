0693734a83c15e61102c5bf579d5d79c32ea51d4
Namra Vora <voranamra625@gmail.com>
2024-09-10 22:19:25 +0530

    add Pattern1

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pattern1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int line=1;line<=num;line++){
+            for(int numberoftimes=1;numberoftimes<=line;numberoftimes++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
