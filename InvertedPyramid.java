79e80a848b990840f7375f405c6bb7e8242b632a
Namra Vora <voranamra625@gmail.com>
2024-09-12 22:37:45 +0530

    add new pattern problems

---
+++ InvertedPyramid.java
@@ -0,0 +1,19 @@
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        int num=4;
+        int space = num-1;
+        int star=1;
+        for(int line=1;line<=num;line++){
+            for(int s=1;s<=space;s++){
+                System.out.print(" ");
+            }
+            for(int st=1;st<=star;st++){
+                System.out.print("*");
+            }
+            space--;
+            star++;
+            
+        System.out.println("");
+        }
+    }
+}
