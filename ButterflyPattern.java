3d4a983c1353797d45389450c892c0884503d441
Namra Vora <voranamra625@gmail.com>
2024-09-17 23:57:50 +0530

    adding ArrayDemo

---
+++ ButterflyPattern.java
@@ -0,0 +1,30 @@
+
+public class ButterflyPattern {
+   
+   public static void main(String[] args) {
+    
+    int num=5;
+    int star=1;
+    int space=num-1;
+
+    for(int i=1;i<=num;i++){
+        for(int starprint=1;starprint<=star;starprint++){
+            System.out.print("*");
+        }
+        for(int spaceprint=1;spaceprint<=space;spaceprint++){
+            System.out.print(" ");
+        }
+        for(int starprint=1;starprint<=star;starprint++){
+            System.out.print("*");
+        }
+        
+    star++;
+    space=-2;
+    
+    System.out.println();
+        
+    }
+   }
+    
+}
+ 
---
+++ InvertedNumPyramid.java
@@ -0,0 +1,13 @@
+public class InvertedNumPyramid {
+    public static void main(String[] args) {
+        int num=5;
+        int tnum=num;
+        for(int line=1;line<=5;line++){
+            for(int print=1;print<=tnum;print++){
+                System.out.print(print);
+            }
+            tnum--;
+            System.out.println();
+        }
+    }
+}
