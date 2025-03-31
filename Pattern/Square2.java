1baca42124285e90a78c798da363923fc29c4be6
unknown <voranamra625@gmail.com>
2025-03-31 15:30:10 +0530

    Another version of square

--- Pattern/Square2.java
+++ Pattern/Square2.java
@@ -1,16 +1,15 @@
 
 import java.util.Scanner;
 
-public class Square {
+public class Square2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number : ");
         int num = sc.nextInt();
-
+        int count =1;
         for(int row=1;row<=num;row++){
-            char ch = 'A';
-            for(int col = 1;col<=num;col++){
-                System.out.print(ch++);
+            for(int col=1;col<=num;col++){
+                System.out.print(count++);
             }
             System.out.println();
         }
