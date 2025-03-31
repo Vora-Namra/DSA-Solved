08e6458a40ee6e5ca8eeec3a832ebae943c2b652
unknown <voranamra625@gmail.com>
2025-03-31 15:20:31 +0530

    Square pattern

--- Pattern/Square.java
+++ Pattern/Square.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Square {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number : ");
         int num = sc.nextInt();
 
         for(int row=1;row<=num;row++){
