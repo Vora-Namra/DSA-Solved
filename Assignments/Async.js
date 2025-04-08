9d75edc8b67d472e97684ef0c55be3504584ac79
unknown <voranamra625@gmail.com>
2025-04-08 13:19:39 +0530

    adding assignments

--- Assignments/Async.js
+++ Assignments/Async.js
@@ -1,3 +1,9 @@
+
+
+
+//sync code gets executed immediately &
+//  Async code gets into Web Apis & once it gets finished it will be shifted to Callback queue & 
+// when cpu gets idle after executing all the synchronous code it will execute particular async code from callback queue
 console.log("sync1");
 
 setTimeout(()=>{
@@ -25,8 +31,37 @@ setTimeout(function(){
     console.log("settimeout outer2")
 },3000);
 
-//sync code gets executed immediately &
-//  Async code gets into Web Apis & once it gets finished it will be shifted to Callback queue & 
-// when cpu gets idle after executing all the synchronous code it will execute particular async code from callback queue
 
 
+
+//Callback hell
+function myOwnSetTimeout(fn,duration){
+    setTimeout(fn,duration);
+}
+
+myOwnSetTimeout(function(){
+    console.log("first thing");
+    myOwnSetTimeout(function(){
+        console.log("second thing");
+    },2000);
+    console.log("first thing 2")
+},2000);
+
+
+//Promisified callback
+function promisified(duration){
+    const p = new Promise(function(res,rej){
+        setTimeout(function(){
+            res("done");
+        },duration);
+    });
+    
+    return p;
+}
+promisified(1000).then((data)=>console.log(data))
+                .catch(err=>console.log(err));
+
+
+
+
+                
--- Assignments/Promise.js
+++ Assignments/Promise.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 function readFile(){
     console.log("Inside Readfile");
-    return new Promise(function(res){
+    return new Promise(function(res,rej){
         console.log("Inside Promise");
         fs.readFile("a.txt","utf-8",function(err,data){
             res(data);
@@ -13,4 +13,32 @@ function onDone(data){
     console.log(data);
 }
 
-readFile().then(onDone);
+readFile().then(onDone)
+        .catch(err=>console.log(err));
+
+
+let n = new Promise(function(res,rej){
+    res("done");
+});
+
+n.then(x=>console.log(x));
+
+
+
+//Promise Chaining
+function promisified(duration){
+    const p = new Promise(function(res,rej){
+        setTimeout(function(){
+            res("done");
+        },duration);
+    });
+    
+    return p;
+}
+
+promisified(1000).then(function(){
+    console.log("First promise done");
+    promisified(2000).then(function(){
+        console.log("Second promise done");
+    })
+})
